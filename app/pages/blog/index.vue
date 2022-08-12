e37f6b883bc8e27f17bfb4db037f40986820c4c9
dlwesemann <101487454+dlwesemann@users.noreply.github.com>
2022-08-12 13:44:18 -0600

    fixes to blog title update

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="blog">
     <div class="py-8 md:py-16 text-center">
-      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Blog</h1>
+      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">Updates</h1>
       <h2 class="text-base md:text-lg lg:text-xl xl:text-2xl">
         SDI Analytics Development Updates
       </h2>
@@ -46,7 +46,7 @@ const Pagination = () => import('@/components/commons/pagination.vue');
 
   head(): MetaInfo {
     return {
-      title: 'Blog',
+      title: 'Updates',
       meta: [
         {
           hid: 'description',
